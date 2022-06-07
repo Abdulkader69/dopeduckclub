120e66f04e6dbd0a375d2c329e8d04c0797ff786
Abdul Kader <abdulkader.69k@gmail.com>
2022-06-08 00:23:27 +0600

    revision done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-import Logo from '../assets/images/main-logo.jpg'
-import About from '../assets/images/about.webp'
+import Logo from '../assets/images/main-logo.png'
+import About from '../assets/images/Xtraterrestrials.png'
 import RoadMap from '../assets/images/Roadmap.webp'
-import NewsLetter from '../assets/images/Newsletter-.webp'
-import WhitePaper from '../assets/images/Whitepaper.webp'
+import NewsLetter from '../assets/images/FAQ.png'
+// import WhitePaper from '../assets/images/Whitepaper.webp'
 import Team from '../assets/images/Team.webp';
 import React, { useState } from 'react'
 import { Link } from 'react-scroll'
@@ -10,18 +10,18 @@ import { Link } from 'react-scroll'
 export const Header = () => {
     const [showMenu, setShowMenu] = useState<boolean>(false);
   return (
-    <header id="main-header" className={`${showMenu ? 'bg-[rgba(35,35,35)]' : 'bg-[rgba(35,35,35)]/60'} h-16 fixed top-0 left-0 w-full z-50 overflow-hidden`}>
-        <div className="w-full max-w-siteWidth m-auto px-4 flex items-center justify-between">
+    <header id="main-header" className={`${showMenu ? 'bg-[rgba(35,35,35)]' : 'bg-[rgba(35,35,35)]/60'} h-20 fixed top-0 left-0 w-full z-50 overflow-hidden`}>
+        <div className="w-full max-w-siteWidth m-auto px-4 flex items-center justify-between h-full">
             <div className="logo">
                 <Link className="cursor-pointer" to="Banner" spy={true} smooth={true} duration={500}><img className='w-[120px]' src={Logo} alt="logo" /></Link>
             </div>
             <div className="menu-items hidden md:block">
                 <ul className='flex items-center justify-end'>
-                    <li className='ml-5'><Link className="cursor-pointer" to="About" spy={true} smooth={true} duration={500}><img src={About} alt="" /></Link></li>
-                    <li className='ml-5'><Link className="cursor-pointer" to="RoadMap" spy={true} smooth={true} duration={500}><img src={RoadMap} alt="" /></Link></li>
-                    <li className='ml-5'><Link className="cursor-pointer" to="NewsLetter" spy={true} smooth={true} duration={500}><img src={NewsLetter} alt="" /></Link></li>
-                    <li className='ml-5'><Link className="cursor-pointer" to="WhitePaper" spy={true} smooth={true} duration={500}><img src={WhitePaper} alt="" /></Link></li>
-                    <li className='ml-5'><Link className="cursor-pointer" to="Team" spy={true} smooth={true} duration={500}><img src={Team} alt="" /></Link></li>
+                    <li className='ml-5'><Link className="cursor-pointer" to="About" spy={true} smooth={true} duration={500}><img className='w-[130px]' src={About} alt="" /></Link></li>
+                    <li className='ml-5'><Link className="cursor-pointer" to="RoadMap" spy={true} smooth={true} duration={500}><img className='w-[100px]' src={RoadMap} alt="" /></Link></li>
+                    {/* <li className='ml-5'><Link className="cursor-pointer" to="WhitePaper" spy={true} smooth={true} duration={500}><img src={WhitePaper} alt="" /></Link></li> */}
+                    <li className='ml-5'><Link className="cursor-pointer" to="Team" spy={true} smooth={true} duration={500}><img className='w-[55px]' src={Team} alt="" /></Link></li>
+                    <li className='ml-5'><Link className="cursor-pointer" to="NewsLetter" spy={true} smooth={true} duration={500}><img className='w-[50px]' src={NewsLetter} alt="" /></Link></li>
                 </ul>
             </div>
             <div className="navigator block md:hidden">
@@ -33,12 +33,12 @@ export const Header = () => {
             </div>
         </div>
         <div className={`mobile-menu-items opacity-100 md:opacity-0 fixed w-full h-[calc(100%-64px)] top-[64px] px-4 bg-[rgba(35,35,35)] ${showMenu ? 'right-0' : '-right-full'}`}>
-            <ul className='block'>
-                <li className='ml-5 my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="About" spy={true} smooth={true} duration={500}><img className='w-full max-w-[14%]' src={About} alt="" /></Link></li>
-                <li className='ml-5 my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="RoadMap" spy={true} smooth={true} duration={500}><img className='w-full max-w-[20%]' src={RoadMap} alt="" /></Link></li>
-                <li className='ml-5 my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="NewsLetter" spy={true} smooth={true} duration={500}><img className='w-full max-w-[20%]' src={NewsLetter} alt="" /></Link></li>
-                <li className='ml-5 my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="WhitePaper" spy={true} smooth={true} duration={500}><img className='w-full max-w-[20%]' src={WhitePaper} alt="" /></Link></li>
-                <li className='ml-5 my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="Team" spy={true} smooth={true} duration={500}><img className='w-full max-w-[14%]' src={Team} alt="" /></Link></li>
+            <ul className='block mt-10'>
+                <li className='my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="About" spy={true} smooth={true} duration={500}><img className='w-[130px]' src={About} alt="" /></Link></li>
+                <li className='my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="RoadMap" spy={true} smooth={true} duration={500}><img className='w-[100px]' src={RoadMap} alt="" /></Link></li>
+                {/* <li className='my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="WhitePaper" spy={true} smooth={true} duration={500}><img className='w-full max-w-[20%]' src={WhitePaper} alt="" /></Link></li> */}
+                <li className='my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="Team" spy={true} smooth={true} duration={500}><img className='w-[55px]' src={Team} alt="" /></Link></li>
+                <li className='my-4'><Link onClick={() => setShowMenu((prevMode) => !prevMode)} className="cursor-pointer py-2" to="NewsLetter" spy={true} smooth={true} duration={500}><img className='w-[50px]' src={NewsLetter} alt="" /></Link></li>
             </ul>
         </div>
     </header>
--- src/components/RoadMap.tsx
+++ src/components/RoadMap.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import roadMapIMG from '../assets/images/Roadmap-01-03-2022.webp'
-// import MintIMG from '../assets/images/Mint-03-03-2022.webp'
-// import AirdropIMG from '../assets/images/Airdrop-03-03-2022.webp'
-// import InvestIMG from '../assets/images/Invest-03-03-2022.webp'
-// import DiscussIMG from '../assets/images/Discuss-03-03-2022.webp'
+import CommunityBuildUp from '../assets/images/CommunityBuildUp.png'
+import Minting from '../assets/images/Minting.png'
+import InitialRewards from '../assets/images/InitialRewards.png'
+import P2EStakingGame from '../assets/images/P2EStakingGame.png'
 // import RewardIMG from '../assets/images/Reward-03-03-2022.webp'
 // import WhatSignIMG from '../assets/images/-03-03-2022.webp'
 
@@ -18,8 +18,8 @@ export const RoadMap = () => {
                 <div className="roadMap-item z-20 w-full flex justify-start pb-5">
                     <div className="w-full sm:w-1/2 pr-4 sm:pr-8 before-right relative">
                         <div className="item-content px-8 md:px-14 py-5 roadmap-bg text-center overflow-hidden rounded-[30px] h-full border-2 border-solid border-Dark shadow-Site">
-                            {/* <img className='m-auto mb-2' src={MintIMG} alt="MintIMG" /> */}
-                            <h2 className="roadMap-headline text-3xl font-bold text-[#fff537] textShadow mb-4">Community Build Up</h2>
+                            <img className='m-auto mb-2 w-full h-auto' src={CommunityBuildUp} alt="MintIMG" />
+                            {/* <h2 className="roadMap-headline text-3xl font-bold text-[#fff537] textShadow mb-4">Community Build Up</h2> */}
                             <p className='text-white text-[15px] textShadow'>The deployment of our engaging earning system requires initial interest, which we plan to generate through the building of a strong community.<br/>
                             We’re eager to build a network of supporters that want to help us achieve our goals, and create something that we can all be proud of. Those that arrive early will be rewarded for their support.
                             </p>
@@ -29,8 +29,8 @@ export const RoadMap = () => {
                 <div className="roadMap-item z-20 w-full flex justify-end pb-5">
                     <div className="w-full sm:w-1/2 sm:pl-8 pr-4 sm:pr-0 before-left relative">
                         <div className="item-content px-8 md:px-14 py-5 roadmap-bg text-center overflow-hidden rounded-[30px] h-full border-2 border-solid border-Dark shadow-Site">
-                            {/* <img className='m-auto mb-2' src={MintIMG} alt="MintIMG" /> */}
-                            <h2 className="roadMap-headline text-3xl font-bold text-[#fff537] textShadow mb-4">Minting</h2>
+                            <img className='m-auto mb-2 w-full max-w-[130px] h-auto' src={Minting} alt="MintIMG" />
+                            {/* <h2 className="roadMap-headline text-3xl font-bold text-[#fff537] textShadow mb-4">Minting</h2> */}
                             <p className='text-white text-[15px] textShadow'>Our mint will be split between a presale and public mint. Early supporters of our project will be rewarded with whitelist spots and guaranteed a mint of our genesis collection. The total supply of our collection will be 3333 NFTs. NFT variations will be revealed a week after mint, with the rarity scale decided by a combination of 10 trait categories, containing 60 total traits. A few lucky supporters will mint our 1/1 unique E.T. NFT’s, which are some of our rarest collectibles. When minting a E.T. you’ll instantly become a V.I.P. member of our community, getting exclusive benefits like winning 10 SOL monthly, free merchandise</p>
                         </div>
                     </div>
@@ -38,8 +38,8 @@ export const RoadMap = () => {
                 <div className="roadMap-item z-20 w-full flex justify-start pb-5">
                     <div className="w-full sm:w-1/2 pr-4 sm:pr-8 before-right relative">
                         <div className="item-content px-8 md:px-14 py-5 roadmap-bg text-center overflow-hidden rounded-[30px] h-full border-2 border-solid border-Dark shadow-Site">
-                            {/* <img className='m-auto mb-2' src={InvestIMG} alt="InvestIMG" /> */}
-                            <h2 className="roadMap-headline text-3xl font-bold text-[#fff537] textShadow mb-4">Initial Rewards</h2>
+                            <img className='m-auto mb-2 w-full max-w-[260px] h-auto' src={InitialRewards} alt="InvestIMG" />
+                            {/* <h2 className="roadMap-headline text-3xl font-bold text-[#fff537] textShadow mb-4">Initial Rewards</h2> */}
                             <p className='text-white text-[15px] textShadow'>Before we initiate plans to build our platform, we’ll take the opportunity to give back to our holders as well as lucky members of our community.<br/>
                             We’ll give away 5 SOL to 10 lucky holders, as well as giving away a total of 100 SOL to random holder. We plan to continue rewarding our holders with giveaways at any given opportunity.</p>
                         </div>
@@ -48,8 +48,8 @@ export const RoadMap = () => {
                 <div className="roadMap-item z-20 w-full flex justify-end pb-5">
                     <div className="w-full sm:w-1/2 sm:pl-8 pr-4 sm:pr-0 before-left before-last relative">
                         <div className="item-content px-8 md:px-14 py-5 roadmap-bg text-center overflow-hidden rounded-[30px] h-full border-2 border-solid border-Dark shadow-Site">
-                            {/* <img className='m-auto mb-2' src={DiscussIMG} alt="DiscussIMG" /> */}
-                            <h2 className="roadMap-headline text-3xl font-bold text-[#fff537] textShadow mb-4">P2E Staking Game</h2>
+                            <img className='m-auto mb-2 w-full max-w-[280px] h-auto' src={P2EStakingGame} alt="DiscussIMG" />
+                            {/* <h2 className="roadMap-headline text-3xl font-bold text-[#fff537] textShadow mb-4">P2E Staking Game</h2> */}
                             <p className='text-white text-[15px] textShadow'>One month after mint, we’ll release our play-to earn game on our website, called Planet X, which will be designed to reward our holders for playing through a passive income stream. Planet X is a staking play-to earn game which consists of staking your Xtra Terrestrials for SOL as a reward. As a team, we are looking forward to a +100% APY. As soon as you stake your NFT’s sit back, relax and watch it take life on Planet X.<br />
                             The rarity of your NFT will determine how much you’ll be able to generate, with 1/1 NFTs and our exclusive ETs yielding the most profits. We would like our game to be fun and engaging, and provide users with an experience like no other on the project on the Solana Blockchain.</p>
                         </div>
